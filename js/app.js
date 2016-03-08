bf2dde6a5c336038bb32933306c45a95bc415f30
echel0n <sickrage.tv@gmail.com>
2016-03-08 16:53:52 +0000

    MVO implementation

--- js/app.js
+++ js/app.js
@@ -1,50 +1,52 @@
-var model = {
-	// Get JSON data from http://apis.is/currency/:source
-	//var gengi = $.get("http://apis.is/currency/arion").then( function( data, status, jqXHR ) { return data; });
-	
-	gengi: [
-		{
-			test : ''
+$(function(){
+	// MVO Model-View-Octopus - MV*
+	var model = {
+		init: function() {
+			//
+		},
+		getCurrencyList: function() {
+			// Láta modelið sækja gögn frá apis.is
+			return $.get("http://apis.is/currency/arion").then( function( data, status, jqXHR ) { return data; });;
 		}
-	]
-};
+	};
 
-var octopus = {
-	init: function() {
-		//currencyListView.init();
-		console.log("test");
-	},
-	getCurrencyList: function() {
-		model.gengi;
+	var octopus = {
+		init: function() {
+			model.init();
+			view.init();
+		},
+		getData: function() {
+			return model.getCurrencyList();
+		},
+		getTemplate: function() {
+			return $.get("js/templates/template.handlebars").then( function( template, status, jqXHR ) { return template; });
+		},
+		getStuff: function() {
+			return $.when( octopus.getTemplate(), octopus.getData());
+		}
+	};
+
+	var view = {
+		init: function() {
+			this.getStuff = octopus.getStuff();
+			view.render();
+		},
+		render: function() {
+			this.getStuff.done( function(template, data){
+				var renderer = Handlebars.compile(template);
+				var result = renderer(data);
+				$('#currency-list').html(result);
+			});
+		}
+	};
+
+	var viewCalculator = {
+		init: function() {
+
+		},
+		render: function() {
+
+		}
 	}
-};
-
-var currencyView = {
-	init: function() {
-
-	},
-
-	render: function() {
-
-	}
-}
-
-var currencyListView = {
-	init: function() {
-		//return $.get("templates/template.handlebars").then( function( template, status, jqXHR ) { return template; });
-		var template = $.get("templates/template.gengi");
-		//this.currencyListElem = document.getElementById('currency-list');
-
-		// render this view
-		this.render(template);
-	},
-	render: function(template) {
-		console.log("test");
-		var renderer = Handlebars.compile(template);
-		var gengi = octopus.getCurrencyList();
-		var result = renderer(gengi);
-		$("#content").html(result);
-	}
-};
-
-octopus.init;
+	octopus.init();
+}());
